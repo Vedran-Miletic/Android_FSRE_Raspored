377c879fd56c472610fdacdd199fa8755150ce09
FranoSaric <franosaric18@gmail.com>
2019-06-05 01:40:08 +0200

    Svi podaci prikupljeni

--- podaci.py
+++ podaci.py
@@ -51,9 +51,14 @@ session.query(Termin).delete()
 
 
 #RAČUNARSTVO
-for i in range(1, 4):
+for i in range(1, 9):
+    if i == 6:
+        continue
     studij = Studiji()
-    studij.naziv = "Racunarstvo"
+    if i == 1 or i == 2 or i == 3:
+        studij.naziv = "Racunarstvo"
+    else:
+        studij.naziv = "Racunarstvo - Diplomski"
     studij.godina = i
     studij.url = "http://intranet.fsre.sum.ba:81/intranetfsr/teamworks.dll/calendar/calendar"+str(i)+"/calendar?ShowSysMessages=true&urlencUTF8=true"
     session.add(studij)
@@ -118,6 +123,71 @@ for i in range(0, 3):
         studij.url = "http://intranet.fsre.sum.ba:81/intranetfsr/teamworks.dll/calendar/calendar44/calendar?"
     else:
         studij.url = "http://intranet.fsre.sum.ba:81/intranetfsr/teamworks.dll/calendar/calendar44/calendar"+str(i)+"?"
+    session.add(studij)
+    session.commit()
+
+
+
+    res = requests.get(studij.url)
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+
+
+    divs = soup.select('.appointment > div.h4 > span')
+    styles = soup.select('.appointment[style]')
+    scripts = soup.select('script')
+    lines = scripts[18].string.split("\n")
+    appointments = {}
+    for idx, line in enumerate(lines):
+        if line.startswith("FActivityArray") and "[\"id\"]" in line:
+            id = line.replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "")
+
+            startDateTime = lines[idx+5].replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "")
+            starTime = lines[idx+5].replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "").split(',')[1]
+            endDateTime = lines[idx+4].replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "")
+            startDateTime = datetime.strptime(startDateTime, "%Y-%m-%d,%H:%M:%S")
+            endDateTime = datetime.strptime(endDateTime, "%Y-%m-%d,%H:%M:%S")
+
+            appointments[id] = {
+                'id': id,
+                'date': startDateTime,
+                'duration': float((endDateTime-startDateTime).seconds/60/60),
+                'startTime': starTime,
+                'studiji.id': studij.godina
+            }
+
+
+
+
+    for id, div in enumerate(divs):
+        style = {}
+        for attr in styles[id]['style'].split(";"):
+            if (len(attr.split(":")) == 2):
+                key, value = attr.split(":")
+                style[key] = value
+        termin = Termin()
+        appointment = appointments[divs[id]['id'].replace("_vertraulich", "")]
+        termin.title = divs[id].text
+        termin.date = appointment['date']
+        termin.duration = appointment['duration']
+        termin.startTime = appointment['startTime']
+        termin.studiji_id = studij.id
+        session.add(termin)
+        session.commit()
+
+#STROJARSTVO
+
+for i in range(6, 29):
+    if i > 6 and i < 16 or i == 19 or i == 23 or i == 27:
+        continue
+
+    studij = Studiji()
+    if i == 6 or i == 16 or i == 17 or i == 18 or i == 20:
+        studij.naziv = "Strojarstvo"
+    else:
+        studij.naziv = "Strojarstvo - Diplomski"
+    studij.godina = i
+    studij.url = "http://intranet.fsre.sum.ba:81/intranetfsr/teamworks.dll/calendar/calendar"+str(i)+"/calendar?"
     session.add(studij)
     session.commit()
 
