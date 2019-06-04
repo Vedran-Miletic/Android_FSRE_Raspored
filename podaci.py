390e85d79971c708035cbcdf0f271cecfdf68bef
FranoSaric <franosaric18@gmail.com>
2019-06-04 22:31:35 +0200

    Update za elektrotehniku

--- podaci.py
+++ podaci.py
@@ -49,6 +49,8 @@ Model.metadata.create_all(engine)
 session.query(Studiji).delete()
 session.query(Termin).delete()
 
+
+#RAČUNARSTVO
 for i in range(1, 4):
     studij = Studiji()
     studij.naziv = "Racunarstvo"
@@ -103,8 +105,68 @@ for i in range(1, 4):
         termin.duration = appointment['duration']
         termin.startTime = appointment['startTime']
         termin.studiji_id = studij.id
-        print(termin.title)
         session.add(termin)
         session.commit()
 
+#ELEKTROTEHNIKA
+
+for i in range(0, 3):
+    studij = Studiji()
+    studij.naziv = "Elektrotehnika"
+    studij.godina = i + 1
+    if i < 1:
+        studij.url = "http://intranet.fsre.sum.ba:81/intranetfsr/teamworks.dll/calendar/calendar44/calendar?"
+    else:
+        studij.url = "http://intranet.fsre.sum.ba:81/intranetfsr/teamworks.dll/calendar/calendar44/calendar"+str(i)+"?"
+    session.add(studij)
+    session.commit()
+
+
+
+    res = requests.get(studij.url)
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+
+
+    divs = soup.select('.appointment > div.h4 > span')
+    styles = soup.select('.appointment[style]')
+    scripts = soup.select('script')
+    lines = scripts[18].string.split("\n")
+    appointments = {}
+    for idx, line in enumerate(lines):
+        if line.startswith("FActivityArray") and "[\"id\"]" in line:
+            id = line.replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "")
+
+            startDateTime = lines[idx+5].replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "")
+            starTime = lines[idx+5].replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "").split(',')[1]
+            endDateTime = lines[idx+4].replace(";", "").split(" = ")[1].replace("\"", "").replace("\r", "")
+            startDateTime = datetime.strptime(startDateTime, "%Y-%m-%d,%H:%M:%S")
+            endDateTime = datetime.strptime(endDateTime, "%Y-%m-%d,%H:%M:%S")
+
+            appointments[id] = {
+                'id': id,
+                'date': startDateTime,
+                'duration': float((endDateTime-startDateTime).seconds/60/60),
+                'startTime': starTime,
+                'studiji.id': studij.godina
+            }
+
+
+
+
+    for id, div in enumerate(divs):
+        style = {}
+        for attr in styles[id]['style'].split(";"):
+            if (len(attr.split(":")) == 2):
+                key, value = attr.split(":")
+                style[key] = value
+        termin = Termin()
+        appointment = appointments[divs[id]['id'].replace("_vertraulich", "")]
+        termin.title = divs[id].text
+        termin.date = appointment['date']
+        termin.duration = appointment['duration']
+        termin.startTime = appointment['startTime']
+        termin.studiji_id = studij.id
+        session.add(termin)
+        session.commit()
 
